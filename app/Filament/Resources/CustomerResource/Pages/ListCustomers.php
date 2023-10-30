a5794c7da68b343488bb94e1b621e44fac1e07e3
tpjigar <tp.jigar@gmail.com>
2023-10-30 19:15:01 +0530

    Add dynamic tabs for Customer table

--- app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use App\Models\PipelineStage;
 use Filament\Actions\CreateAction;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCustomers extends ListRecords
@@ -15,5 +18,25 @@ class ListCustomers extends ListRecords
         return [
             CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+
+        $tabs['all'] = Tab::make('All Customers')
+            ->badge(Customer::count());
+
+        $pipelineStages = PipelineStage::orderBy('position')->withCount('customers')->get();
+
+        foreach ($pipelineStages as $pipelineStage) {
+            $tabs[str($pipelineStage->name)->slug()->toString()] = Tab::make($pipelineStage->name)
+                ->badge($pipelineStage->customers_count)
+                ->modifyQueryUsing(function ($query) use ($pipelineStage) {
+                    return $query->where('pipeline_stage_id', $pipelineStage->id);
+                });
+        }
+
+        return $tabs;
     }
 }
